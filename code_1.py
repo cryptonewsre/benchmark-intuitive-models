3dd444d3d38c3d811cb90d7a011ae7d5b43989ea
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-10-28 21:13:04 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,25 @@
+    }
+        int[] data = new int[10];
+
+        }
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        }
+}
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+
+        }
+        }
+        int[] data = new int[10];
+        for (int item : data) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+    }
+
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+public class RandomDataGenerator {
